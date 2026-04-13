e644338e983911afb700bc6a1ad1233cb8a603fd
Jazzoon <meles.jason19@gmail.com>
2026-04-13 18:34:10 +0100

    CRUD complet players : ajout route PUT update

--- app/routers/players.py
+++ app/routers/players.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.schemas.player import PlayerCreate
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter(
     prefix="/players",
@@ -16,3 +17,25 @@ async def create_player(player: PlayerCreate):
 @router.get("/")
 async def get_players():
     return {"players": players_db}
+
+@router.get("/{player_id}")
+async def get_player(player_id: int):
+    if player_id < 0 or player_id >= len(players_db):
+        raise HTTPException(status_code=404, detail="Joueur introuvable")
+    return {"player": players_db[player_id]}
+
+@router.delete("/{player_id}")
+async def delete_player(player_id: int):
+    if player_id < 0 or player_id >= len(players_db):
+        raise HTTPException(status_code=404, detail="Joueur introuvable")
+    deleted_player = players_db[player_id]["username"]
+    players_db.pop(player_id)
+    return {"message": f"Joueur {deleted_player} a été supprimé"}
+
+@router.put("/{player_id}")
+async def update_player(player_id: int, player: PlayerCreate):
+    if player_id < 0 or player_id >= len(players_db):
+        raise HTTPException(status_code=404, detail="Joueur introuvable")
+    old_username = players_db[player_id]["username"]
+    players_db[player_id] = player.model_dump()
+    return {"message": f"Joueur {old_username} mis à jour", "player": players_db[player_id]}
