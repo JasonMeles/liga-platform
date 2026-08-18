3a80da22195b2efb03fe48ec76a03345bb5546f5
Jazzoon <meles.jason19@gmail.com>
2026-08-18 13:59:02 +0200

    Ajout handler d'erreurs global (exception_handler) pour capturer les erreurs non prévues

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ from contextlib import asynccontextmanager
 from app.database.connection import get_db
 from app.models.player import Player, PlayerTypeEnum
 from sqlalchemy import select
-
+from fastapi import Request
+from fastapi.responses import JSONResponse
+import logging
 
 
 @asynccontextmanager
@@ -26,12 +28,15 @@ async def lifespan(app: FastAPI):
     yield
 
 logging_config.setup_logging()
+
 app = FastAPI(
     title="Liga Platform API",
     description="Backend pour la gestion de ligues gaming",
     version="0.1.0",
     lifespan=lifespan
 )
+
+logger = logging.getLogger(__name__)
 
 app.include_router(players.router) 
 app.include_router(auth.router)
@@ -41,7 +46,6 @@ app.include_router(matches.router)
 app.include_router(feed.router)
 app.include_router(websockets.router)
 
-
 @app.get("/")
 async def root():
     return {"message": "Liga Platform is alive 🎮"}
@@ -50,3 +54,10 @@ async def root():
 async def health():
     return {"status": "ok"}
 
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Erreur non gérée sur {request.url}: {exc}")
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Une erreur interne est survenue"}
+    )
